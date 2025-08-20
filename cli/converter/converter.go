729750596341115bcbf633320c12d3382196d845
Tachera Sasi <tacherasasi@gmail.com>
2025-08-20 09:46:12 +0300

    Improve error handling in Png2jpeg function for PNG decoding and JPEG file creation

--- cli/converter/converter.go
+++ cli/converter/converter.go
@@ -14,19 +14,25 @@ func Png2jpeg(input string) error {
 	if err != nil {
 		return err
 	}
+	defer image.Close()
 
 	pngImage, err := png.Decode(image)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to decode PNG image: %v", err)
 	}
 
 	jpegFile, err := os.Create(output)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to create JPEG file: %v", err)
 	}
 	defer jpegFile.Close()
-	defer image.Close()
+
+	if err := jpeg.Encode(jpegFile, pngImage, nil); err != nil {
+		return fmt.Errorf("failed to encode JPEG image: %v", err)
+	}
+
 	fmt.Printf("Converting %s to %s format...\n", input, output)
+	
 	return nil
 }
 
